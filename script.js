f79f95c447bd828cd1b67759341cb9d85285df5f
aspittel <ali07cat07@gmail.com>
2017-10-23 21:17:25 -0400

    towers of hanoi

---
+++ script.js
@@ -0,0 +1,47 @@
+let moves = 0
+let moveDisc = false
+let selectedDock = null
+let origDiv = null
+
+let scoreDiv = document.getElementById("#score")
+
+function getFirstDisc (div) {
+    return div.getElementsByTagName("div")[0]
+}
+
+function checkDiscSizes (divOne, divTwo) {
+    return getFirstDisc(divOne).dataset.size > divTwo.dataset.size
+}
+
+function checkIfDisc (div) {
+    return div.getElementsByTagName("div").length === 0
+}
+
+function selectDock (div) {
+    selectedDock = getFirstDisc(div)
+    origDiv = div
+    div.className += " selected"
+}
+
+function moveToDock (div) {
+    if (checkIfDisc(div) || checkDiscSizes(div, selectedDock)) {
+        moves ++
+        div.insertBefore(selectedDock, div.firstChild)
+        score.innerHTML = moves
+    }
+    origDiv.classList.remove("selected")
+}
+
+function handleClick (e) {
+    if (moveDisc) {
+        moveToDock(this)
+    } else {
+        selectDock(this)
+    }
+    moveDisc = !moveDisc
+}  
+
+Array.from(document.getElementsByClassName("dock")).forEach(dock => {
+    dock.addEventListener("click", handleClick)
+})
+
